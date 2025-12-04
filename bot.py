a63538992962748f79767a61bfe0ac3ec9c976d4
yuji1320 <b7190853@gmail.com>
2025-12-04 16:33:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-from telebot import TeleBot, apihelper
-
-# توکن ربات تلگرام
-TOKEN = "8592104314:AAGVoXhgaHqwxx4aXW-MhjTot8Psah3iHSQ"
+from telebot import TeleBot
 
-# Chat ID شما (ادمین)
+TOKEN = "توکن جدید"
 ADMIN_ID = 7914359763
 
 bot = TeleBot(TOKEN)
 
-# هر پیام از کاربر → ارسال به ادمین
+bot.remove_webhook()  # حذف وبهوک در صورت فعال بودن قبلی
+
 @bot.message_handler(func=lambda m: True)
 def forward_to_admin(message):
     bot.forward_message(ADMIN_ID, message.chat.id, message.message_id)
